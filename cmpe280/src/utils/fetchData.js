0deeda72b8ad2035a6782dca86a8ea00a08f349f
Shraddha Pathradkar <shraddhapathradkar9@gmail.com>
2022-11-26 23:51:14 -0800

    errors resolved

---
+++ cmpe280/src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '09cb7494a3msh33703ce603ec4c2p1ce674jsn661dec0c379f',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
