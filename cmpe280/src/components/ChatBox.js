8580ac811c0c6b2ff5d123fd4da5746e6abf0f2c
Shraddha Pathradkar <shraddhapathradkar9@gmail.com>
2022-11-28 11:00:10 -0800

    css changed chat bot

--- cmpe280/src/components/ChatBox.js
+++ cmpe280/src/components/ChatBox.js
@@ -4,47 +4,63 @@ import { ThemeProvider } from "styled-components";
 
 class Chatbox extends React.Component {
   render() {
-    return (
-      <ChatBot
-        steps={[
-          {
-            id: "1",
-            message: "Hola! need Help?",
-            trigger: "2"
-          },
-          {
-            id: "2",
-            user: true,
-            trigger: "3"
-          },
-          {
-            id: "3",
-            message: "{previousValue}, okay",
-            trigger: "4"
-          },
+    const theme = {
+      background: '#f5f8fb',
+      fontFamily: 'Helvetica Neue',
+      headerBgColor: 'red',
+      headerFontColor: '#fff',
+      headerFontSize: '15px',
+      botBubbleColor: 'red',
+      botFontColor: '#fff',
+      userBubbleColor: '#fff',
+      userFontColor: '#4a4a4a',
+    };
 
-          {
-            id: "4",
-            user: true,
-            trigger: "5"
-          },
-          {
-            id: "5",
-            message: "okay",
-            trigger: "6"
-          },
-          {
-            id: "6",
-            user: true,
-            trigger: "7"
-          },
-          {
-            id: "7",
-            message: "okay",
-            end: true
-          }
-        ]}
-      />
+    return (
+      <ThemeProvider theme={theme}>
+
+        <ChatBot
+
+          steps={[
+            {
+              id: "1",
+              message: "Hola! need Help?",
+              trigger: "2"
+            },
+            {
+              id: "2",
+              user: true,
+              trigger: "3"
+            },
+            {
+              id: "3",
+              message: "{previousValue}, okay",
+              trigger: "4"
+            },
+
+            {
+              id: "4",
+              user: true,
+              trigger: "5"
+            },
+            {
+              id: "5",
+              message: "okay",
+              trigger: "6"
+            },
+            {
+              id: "6",
+              user: true,
+              trigger: "7"
+            },
+            {
+              id: "7",
+              message: "okay",
+              end: true
+            }
+          ]}
+        />
+      </ThemeProvider>
     );
   }
 }
